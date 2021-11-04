33859831ab05c073b1b4eb1fc81540d0b637a90f
Aurelie715 <aureliebernard29@gmail.com>
2021-11-04 14:51:29 +0100

    Implémenter entrées du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,15 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const close = document.querySelectorAll(".close")
+const close = document.querySelectorAll(".close");
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const quantity = document.getElementById('quantity');
+const locations = document.querySelectorAll('input[name="location"]');
+const submit = document.querySelector(".btn-submit");
+const checkbox = document.getElementById('checkbox1')
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -28,6 +36,33 @@ close.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+
+
+submit.addEventListener("click", function(event) {
+  let valid = true;
+  if (!/^[a-zA-Z]{2,}$/.test(firstName.value)){
+    valid = false;
+  }
+  if (!/^[a-zA-Z]{2,}$/.test(lastName.value)){
+    valid = false;
+  }
+  if(!/^\S+@\S+\.\S+$/.test(email.value)){
+    valid = false;
+  }
+  if (quantity.value == "") {
+    valid = false;
+  }
+  if (!document.querySelector('input[name="location"]:checked')) {
+    valid = false;
+  }
+  if (!checkbox1.checked) {
+    valid = false;
+  }
+  if (!valid) {event.preventDefault();}
+});
+
+
 
 
 
