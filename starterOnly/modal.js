b5e9749cb5b917a968743b5f2dd3a670a6d0c29c
Aurelie715 <aureliebernard29@gmail.com>
2021-11-17 19:58:25 +0000

    css modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,10 +8,12 @@ function editNav() {
 }
 
 // DOM Elements
+const form = document.getElementById("form");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const content = document.querySelectorAll(".content")
 const formData = document.querySelectorAll(".formData");
-const close = document.querySelectorAll(".close");
+const closeModalBtn = document.querySelectorAll(".close, .close-form-success");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
@@ -27,6 +29,7 @@ const quantityError = quantity.nextElementSibling;
 const locationError = document.getElementById("location-error");
 const checkboxError = document.getElementById("checkbox-error");
 const birthdateError = birthdate.nextElementSibling;
+const formSuccess = document.querySelector(".form-success");
 
 
 // launch modal event
@@ -35,52 +38,67 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+
+  form.style.display = "block";
+  formSuccess.style.display= "none";
 }
 
 // close modal event
-close.forEach((btn) => btn.addEventListener("click", closeModal));
+closeModalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+  // document.getElementById(form).reset();
 }
 
 form.addEventListener("submit", function (event) {
+  event.preventDefault();
   let valid = true;
   if (!/^[a-zA-Z]{2,}$/.test(firstName.value)) {
     valid = false;
     firstNameError.style.display = "block";
     firstNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    firstName.classList.add("invalid-field");
   } else {
     firstNameError.style.display = "none";
+    firstName.classList.remove("invalid-field");
   }
   if (!/^[a-zA-Z]{2,}$/.test(lastName.value)) {
     valid = false;
     lastNameError.style.display = "block";
     lastNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+    lastName.classList.add("invalid-field");
   } else {
     lastNameError.style.display = "none";
+    lastName.classList.remove("invalid-field");
   }
   if (!/^\S+@\S+\.\S+$/.test(email.value)) {
     valid = false;
     emailError.style.display = "block";
     emailError.textContent = "Veuillez entrer une adresse email valide.";
+    email.classList.add("invalid-field");
   } else {
     emailError.style.display = "none";
+    email.classList.remove("invalid-field")
   }
   if (birthdate.value == "") {
     valid = false;
     birthdateError.style.display = "block";
     birthdateError.textContent = "Vous devez entrer votre date de naissance.";
+    birthdate.classList.add("invalid-field");
   } else {
     birthdateError.style.display = "none";
+    birthdate.classList.remove("invalid-field");
   }
   if (!/^[1-9][0-9]?$/.test(quantity.value)) {
     valid = false;
     quantityError.style.display = "block";
     quantityError.textContent = "Veuillez entrer un nombre entre 0 et 99.";
+    quantity.classList.add("invalid-field");
   } else {
     quantityError.style.display = "none";
+    quantity.classList.remove("invalid-field");
   }
   if (!document.querySelector('input[name="location"]:checked')) {
     valid = false;
@@ -89,16 +107,22 @@ form.addEventListener("submit", function (event) {
   } else {
     locationError.style.display = "none";
   }
-  if (!checkbox1.checked) {
+  if (!checkbox.checked) {
     valid = false;
     checkboxError.style.display = "block";
     checkboxError.textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
   } else {
     checkboxError.style.display = "none";
   }
-  if (!valid) {
-    event.preventDefault();
-  }
+  formSuccess.style.display = valid ? 'block' : 'none';
+  form.style.display = valid ? 'none' : 'block';
+  
+  
+  
+  // else {
+  //   modalbg.innerHTML = "<p> Thank you for submitting your registration details</p>"
+  // }
+  
 
 //   const form = document.getElementById("form");
 // let formDatas = new FormData(form)
